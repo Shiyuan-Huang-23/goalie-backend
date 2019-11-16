810859903158fc34d99e13195643058b364ca611
Shiyuan Huang <sh2223@cornell.edu>
2019-11-16 17:36:03 -0500

    Added more fields to StudyGroup

--- src/db.py
+++ src/db.py
@@ -7,21 +7,33 @@ class StudyGroup(db.Model):
     __tablename__ = 'study_group'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
     time = db.Column(db.DateTime, nullable=False)
+    duration = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     # TODO participants/users
     # participants = db.relation('User', secondary=participant_association_table, backpopulates='study_groups')
 
     def __init__(self, **kwargs):
         self.name = kwargs.get('name', 'Unnamed Study Group')
         # TODO change this to allow users to set a date and time
+        self.date = kwargs.get('date', datetime.datetime.now())
         self.time = datetime.datetime.utcnow
+        self.duration = kwargs.get('duration', 1)
+        self.location = kwargs.get('location', 'Nowhere')
+        self.description = kwargs.get('description', '')
         # self.participants = []
     
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
-            'time': self.time
+            'date': self.date.strftime("%m/%d/%y"),
+            'time': self.time.strftime("%h:%m"),
+            'duration': self.duration,
+            'location': self.location,
+            'description': self.description
             # TODO serialize participants
         }
 
